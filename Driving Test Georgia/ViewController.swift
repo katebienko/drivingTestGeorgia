9d380ab26f98ca23782dfff8fabba0190e20e680
katebienko <108893252+katebienko@users.noreply.github.com>
2022-10-19 22:03:26 +0400

    Add Navigation

--- Driving Test Georgia/ViewController.swift
+++ Driving Test Georgia/ViewController.swift
@@ -8,6 +8,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setDesignViews()
+        tapViewCategoryBB1()
+    }
+    
+    private func tapViewCategoryBB1() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        viewCategoryBB1.isUserInteractionEnabled = true
+        viewCategoryBB1.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+       
+        if let testViewController = storyboard.instantiateViewController(identifier: "TestViewController") as? TestViewController {
+            testViewController.modalPresentationStyle = .fullScreen
+            navigationController?.pushViewController(testViewController, animated: true)
+        }
     }
     
     private func setDesignViews() {
