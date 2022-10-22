917bed701e2ce10c49b235d457a87abf533edf29
katebienko <108893252+katebienko@users.noreply.github.com>
2022-10-22 15:56:09 +0400

    Show random position of answers

--- Driving Test Georgia/CardCollectionViewCell.swift
+++ Driving Test Georgia/CardCollectionViewCell.swift
@@ -2,17 +2,28 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
 
+    var answersArray: [String] = []
+
     @IBOutlet weak var question: UILabel!
     @IBOutlet weak var answer1: UILabel!
-    @IBOutlet weak var answer2: UILabel!
+    @IBOutlet weak var answer2: UILabel!    
+    @IBOutlet weak var answer3: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func setup(with tickets: Tickets) {
+        
+        for i in 0 ... tickets.answers.count - 1 {
+            answersArray.append(tickets.answers[i].text)
+        }
+        
+        answersArray.shuffle()
+        
         question.text = tickets.question
-        answer1.text = tickets.answers[0].text
-        answer2.text = tickets.answers[1].text
+        answer1.text = answersArray[0]
+        answer2.text = answersArray[1]
+        answer3.text = answersArray[2]
     }
 }
