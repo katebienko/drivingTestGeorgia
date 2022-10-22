c4af27ebaadcce406449a2de127ee1f2491622e5
katebienko <108893252+katebienko@users.noreply.github.com>
2022-10-22 17:53:18 +0400

    Tuples added

--- Driving Test Georgia/CardCollectionViewCell.swift
+++ Driving Test Georgia/CardCollectionViewCell.swift
@@ -2,10 +2,10 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
 
-    var answersArray: [String] = []
+    var answersArray: [(question: String, correct: Bool)] = []
 
     @IBOutlet weak var question: UILabel!
-    @IBOutlet weak var answer1: UILabel!
+    @IBOutlet weak var answer1: UIButton!
     @IBOutlet weak var answer2: UILabel!    
     @IBOutlet weak var answer3: UILabel!
     
@@ -14,16 +14,23 @@ class CardCollectionViewCell: UICollectionViewCell {
     }
     
     func setup(with tickets: Tickets) {
-        
         for i in 0 ... tickets.answers.count - 1 {
-            answersArray.append(tickets.answers[i].text)
+            answersArray.append((tickets.answers[i].text, tickets.answers[i].correct))
         }
-        
         answersArray.shuffle()
+        print(answersArray)
         
         question.text = tickets.question
-        answer1.text = answersArray[0]
-        answer2.text = answersArray[1]
-        answer3.text = answersArray[2]
+        answer1.setTitle(answersArray[0].question, for: .normal)
+        answer2.text = answersArray[1].question
+        answer3.text = answersArray[2].question
+    }
+    
+    @IBAction func answerTap(_ sender: Any) {
+        if answersArray[0].correct == true {
+            answer1.backgroundColor = .green
+        } else {
+            answer1.backgroundColor = .red
+        }
     }
 }
